d198580999b66f777844cc37514123b67db27dae
Kyle Kobayashi <kobayashi14k@gmail.com>
2020-02-04 14:04:50 -0600

    Added Boolean to new method

--- app/src/main/java/com/example/pushup/MainActivity.java
+++ app/src/main/java/com/example/pushup/MainActivity.java
@@ -16,9 +16,9 @@ public class MainActivity extends AppCompatActivity {
 
         int i = 1+1;
 
-        int ii = 3+3;
+        int ii = 3*3;
 
-        int iii = i + ii;
+        int iii = i * ii;
 
         String test = "Test";
 
